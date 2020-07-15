da4acdbd546c34e1431ed6483994b84df776a4cf
John Easter <john@owltestprep.com>
2020-07-15 15:47:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -28,7 +28,8 @@ def aoh_lookup(aoh = assembled_aoh, row = 2, key = occupation)
   assembled_aoh[row][key]
 end
 
-def aoh_update(aoh, row, key, new_value)
+def aoh_update(aoh = assembled_aoh, row = 2, key = occupation, new_value = "Dying")
+  assembled_aoh[row][key] = new_value
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
